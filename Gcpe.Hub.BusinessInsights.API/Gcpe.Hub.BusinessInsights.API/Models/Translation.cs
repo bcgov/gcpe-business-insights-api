2eb56dd920b0d7df03be728ac9be11a2fca84496
Lashley <Andre.Lashley@gov.bc.ca>
2023-03-24 15:54:07 -0700

    Add release type to dto

--- Gcpe.Hub.BusinessInsights.API/Gcpe.Hub.BusinessInsights.API/Models/Translation.cs
+++ Gcpe.Hub.BusinessInsights.API/Gcpe.Hub.BusinessInsights.API/Models/Translation.cs
@@ -9,6 +9,7 @@ namespace Gcpe.Hub.BusinessInsights.API.Models
         public string Headline { get; set; } = string.Empty;
         public string Ministry { get; set; } = string.Empty;
         public List<string> Urls { get; set; } = new List<string>();
+        public int ReleaseType { get; set; }
         public DateTimeOffset PublishDateTime { get; set; }
     }
 }
